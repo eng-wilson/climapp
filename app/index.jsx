61e1d22e06899fa2ba5a5fcaefe2defaa0563fe4
Wilson Carvalho <eng.wilson.c@gmail.com>
2025-01-18 14:40:08 -0300

    style: home screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,63 @@
-import { View, Text } from "react-native";
+import { Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <LinearGradient colors={["#00457D", "#05051F"]} style={styles.container}>
+      <Image
+        source={require("../assets/images/Logo.png")}
+        resizeMode="contain"
+        style={styles.logo}
+      />
+      <Image
+        source={require("../assets/images/image1.png")}
+        resizeMode="cover"
+        style={styles.image}
+      />
+
+      <Text style={styles.screenTitle}>Boas-vindas!</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
+    </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 32,
+    paddingVertical: 79,
+    gap: 64,
+  },
+  logo: {
+    width: 147,
+    height: 45,
+  },
+  image: {
+    width: 190,
+    height: 180,
+  },
+  button: {
+    backgroundColor: "#7693FF",
+    height: 40,
+    width: "100%",
+    borderRadius: 32,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  screenTitle: {
+    color: "#fff",
+    fontSize: 25,
+  },
+  buttonTitle: {
+    color: "##01080E",
+    fontSize: 20,
+    fontWeight: "semibold",
+  },
+});
 
 export default Home;
