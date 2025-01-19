a15c8cad04ce29e6596720e1f5b8283773db38c5
Wilson Carvalho <eng.wilson.c@gmail.com>
2025-01-19 16:58:34 -0300

    style: cities list added

--- app/cities.jsx
+++ app/cities.jsx
@@ -1,10 +1,52 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { Text, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  Text,
+  Image,
+} from "react-native";
 
 const Cities = () => {
+  const cities = [
+    { city: "São Paulo", state: "SP", temperature: "16º" },
+    { city: "Rio de Janeiro", state: "RJ", temperature: "20º" },
+    { city: "Belo Horizonte", state: "MG", temperature: "18º" },
+    { city: "Salvador", state: "BA", temperature: "22º" },
+    { city: "Brasília", state: "DF", temperature: "16º" },
+    { city: "Curitiba", state: "PR", temperature: "16º" },
+    { city: "Fortaleza", state: "CE", temperature: "24º" },
+    { city: "Manaus", state: "AM", temperature: "26º" },
+    { city: "Recife", state: "PE", temperature: "28º" },
+    { city: "Belém", state: "PA", temperature: "30º" },
+    { city: "Goiânia", state: "GO", temperature: "25º" },
+    { city: "São Luís", state: "MA", temperature: "36º" },
+  ];
+
   return (
     <LinearGradient colors={["#00457D", "#05051F"]} style={styles.container}>
-      <Text>Cidades</Text>
+      <View>
+        <TextInput placeholder="Digite a cidade" />
+      </View>
+
+      <FlatList
+        data={cities}
+        contentContainerStyle={styles.listContainer}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Image
+              source={require("../assets/images/image1.png")}
+              style={styles.image}
+            />
+            <Text style={styles.cityName}>
+              {item.city} - {item.state}
+            </Text>
+
+            <Text style={styles.temperature}>{item.temperature}</Text>
+          </View>
+        )}
+      />
     </LinearGradient>
   );
 };
@@ -14,6 +56,34 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    paddingHorizontal: 16,
+  },
+  listContainer: {
+    width: "100%",
+    gap: 16,
+  },
+  listItem: {
+    width: "100%",
+    height: 64,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 16,
+    backgroundColor: "rgba(255, 255, 255, 0.15)",
+    borderRadius: 16,
+  },
+  image: {
+    width: 26,
+    height: 26,
+  },
+  cityName: {
+    fontSize: 16,
+    color: "#fff",
+  },
+  temperature: {
+    fontSize: 25,
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
 
